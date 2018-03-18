0bea54607357445dedfd550784d6116408350518
GeniusK <geniusk@rinc.kr>
2018-03-18 13:04:43 +0900

    슬라이드 수정

--- Crystal/2018-03-04/script.js
+++ Crystal/2018-03-04/script.js
@@ -8,44 +8,27 @@ $(function () {
   }, TIMER)
 
   // click -> hover로 하면 임블리처럼 효과가 변함
-  $('.slide-pointer-area > span').click(function () {
-    $('.slide-pointer-area > span').css('background-color', '#e4c9a6')
-    $(this).css('background-color', '#222222')
-    imageCount = $(this).index()
-    slideImage()
-    clearInterval(slideInterval)
-    slideInterval = setInterval(function () {
-      slideImage()
-    }, TIMER)
-  })
+  // $('.slide-pointer-area > span').click(function () {
+  //   $('.slide-pointer-area > span').css('background-color', '#e4c9a6')
+  //   $(this).css('background-color', '#222222')
+  //   imageCount = $(this).index()
+  //   slideImage()
+  //   clearInterval(slideInterval)
+  //   slideInterval = setInterval(function () {
+  //     slideImage()
+  //   }, TIMER)
+  // })
+  $('.index-wrap').html('1 / ' + imageChildren.length)
 
   function slideImage() {
     $('.slide-wrap ul').css('left', imageCount * -60 + 'vw')
     $('.slide-pointer-area > span').css('background-color', '#e4c9a6')
     $('.slide-pointer-area > span:nth-of-type(' + (++imageCount) + ')').css('background-color', '#222222')
+    $('.index-wrap').html(imageCount + ' / ' + imageChildren.length)
     if (imageCount === imageChildren.length) {
       imageCount = 0
     }
   }
 
-  $('.slide-wrap img').click(function () {
-    if ($(this).attr('alt') === "left") {
-      imageCount--
-    } else {
-      imageCount++
-    }
-    if (imageCount < 0) {
-      imageCount = 4
-    }
-    if (imageCount > 4) {
-      imageCount = 0
-    }
-    $('.slide-wrap ul').css('left', imageCount * -60 + 'vw')
-    $('.slide-pointer-area > span').css('background-color', '#e4c9a6')
-    $('.slide-pointer-area > span:nth-of-type(' + (imageCount + 1) + ')').css('background-color', '#222222')
-    clearInterval(slideInterval)
-    slideInterval = setInterval(function () {
-      slideImage()
-    }, TIMER)
-  })
+  // $('.slide-wrap img').click(function () {¡
 })
